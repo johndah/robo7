e3513c69f225bb5637dd26659a1cf7c74b0c5413
John Dahlberg <johndah@kth.se>
2018-10-12 15:31:30 +0200

    Update pathplanning.cpp

--- planning/pathplanning/src/pathplanning.cpp
+++ planning/pathplanning/src/pathplanning.cpp
@@ -10,8 +10,10 @@ class PathPlanning;
 class Node
 {
     public:
-        float pi;
-        float x, y, theta, control, time, path_x, path_y, path_cost, cost_to_come;
+        float x, y, theta;
+        float control, time;
+        float path_x, path_y, path_cost, cost_to_come;
+        float tolerance_radius, tolerance_angle;
 
         Node(float x, float y, float theta, float control, float time, float path_x, float path_y, float path_cost, float cost_to_come)
         {
@@ -28,9 +30,9 @@ class Node
             this->parent = Null
             this->successor = Null
 
+            this->tolerance_radius = 1e-1;
+            this->tolerance_angle = pi/2;
         }
-
-
 };
 
 double control_frequency = 10.0;
@@ -52,12 +54,12 @@ public:
 
     robot_position = nh.subscribe("/deadreckogning/Pos", 1000, &PathPlanning::getPositionCallBack, this);
 
-
+    
     pi = 3.14159265358979323846;
 
     //Other parameters
 
-
+    
     dt = 1/control_frequency;
 
     //robot_position = nh.advertise<geometry_msgs::Twist>("Pos", 1000);
@@ -78,19 +80,19 @@ public:
       return 2*node.x;
   }
 
-
+ 
 
   void updatePosition(){
     //Generate the future published twist msg
     geometry_msgs::Twist twist_msg;
 
-
+  
 
     //robot_position.publish(twist_msg);
   }
 
 private:
-
+  
   float pi, dt;
 };
 
@@ -108,7 +110,7 @@ int main(int argc, char **argv)
     while(path_planning.nh.ok())
     {
         randomInt = path_planning.getPath();
-
+        
         ROS_INFO("x: %f", randomInt);
 
         ros::spinOnce();
