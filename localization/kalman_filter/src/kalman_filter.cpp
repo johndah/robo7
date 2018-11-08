efd224dee015bb63570c63738a0b5d036f545fa0
jtao <jtao@kth.se>
2018-11-08 16:03:46 +0100

    cs

--- localization/kalman_filter/src/kalman_filter.cpp
+++ localization/kalman_filter/src/kalman_filter.cpp
@@ -160,8 +160,10 @@ public:
     //Dead_reckoning part
     timeUpdate();
 
+    ROS_INFO("Time in seconds : %lf, %lf, %lf", ros::Time::now().sec + ((float)ros::Time::now().nsec / pow(10, 9)), prev_mes_time, init_time);
+
     //If we didn't get any measures for a while (and the previous one has already been received)
-    if((ros::Time::now().sec + ((float)ros::Time::now().nsec / pow(10, 9)) - prev_mes_time > time_threshold)&&(previous_measure_received)&&use_measure&&(ros::Time::now().sec + ros::Time::now().nsec / pow(10, 9) - init_time > 5))
+    if((ros::Time::now().sec + ((float)ros::Time::now().nsec / pow(10, 9)) - prev_mes_time > time_threshold)&&(previous_measure_received)&&use_measure&&(ros::Time::now().sec + ((float)ros::Time::now().nsec / pow(10, 9)) - init_time > 5))
     {
       ROS_INFO("Time in seconds : %lf, %lf", ros::Time::now().sec + ((float)ros::Time::now().nsec / pow(10, 9)), prev_mes_time);
       // ROS_INFO("Asking for measure");
