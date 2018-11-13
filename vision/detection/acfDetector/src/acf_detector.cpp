b13325416016aa1bc854671fcfff81899a08c13f
jtao <jtao>
2018-11-13 23:12:58 +0100

    svm color classifier

--- vision/detection/acfDetector/src/acf_detector.cpp
+++ vision/detection/acfDetector/src/acf_detector.cpp
@@ -5,13 +5,18 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <geometry_msgs/Point.h>
 
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/highgui/highgui.hpp>
+#include <opencv/cv.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/ml.hpp>
 
 #include <acf/ACF.h>
+#include "acfDetector/detectedObj.h"
 
 using namespace std;
 using namespace cv;
+using namespace cv::ml;
 namespace acf {
     class Detector;
     class ObjectDetector;
@@ -21,17 +26,24 @@ using ObjectDetectorPtr = std::shared_ptr<acf::ObjectDetector>;
 using AcfPtr = std::shared_ptr<acf::Detector>;
 using RectVec = std::vector<cv::Rect>;
 
+// Load the svm color classifier
+int width = 80, height = 80;
+std::string svmModel = "/home/ras17/catkin_ws/src/robo7/vision/detection/acfDetector/model/SVMmodel.xml";
+Ptr<SVM>svm = ml::SVM::load(svmModel);
+
 class ACFdetector
 {
 public:
 	ros::NodeHandle nh_;
 	image_transport::ImageTransport it_;
 	image_transport::Subscriber image_sub_;
-  image_transport::Publisher obj_img_pub;
+  // image_transport::Publisher obj_img_pub;
 
   ros::NodeHandle n;
   ros::Subscriber depth_points_sub;
-  ros::Publisher obj_pos_pub;
+  // ros::Publisher obj_pos_pub;
+
+  ros::Publisher obj_detected_pub;
 
   	ACFdetector()
   	  : it_(nh_)
@@ -39,25 +51,25 @@ public:
   		image_sub_ = it_.subscribe("/camera/rgb/image_rect_color", 1,
   			                       &ACFdetector::detection, this);
 
-      obj_img_pub = it_.advertise("/vision/object/img", 1);
+      // obj_img_pub = it_.advertise("/vision/object/img", 1);
 
   		namedWindow("Detected image");
+      namedWindow("bbx");
       // namedWindow("Cropped object image");
 
       depth_points_sub = n.subscribe("/camera/depth_registered/points", 1, &ACFdetector::depthCallBack, this);
-      obj_pos_pub = n.advertise<geometry_msgs::Point>("/vision/object/pos", 5);
+      // obj_pos_pub = n.advertise<geometry_msgs::Point>("/vision/object/pos", 5);
+      obj_detected_pub = n.advertise<acfDetector::detectedObj>("/vision/object", 1);
 
-
-      std::string sModel;
-      n.param<string>("/acf_detector/sModel", sModel, "/home/jtao/catkin_ws/src/robo7/vision/detection/acfDetector/model/model_color_mag.cpb");
+      std::string acfModel;
+      n.param<string>("/acf_detector/acfModel", acfModel, "/home/ras17/catkin_ws/src/robo7/vision/detection/acfDetector/model/model_color_mag.cpb");
 
       // std::string sModel = "/home/jtao/CLionProjects/lab1/model_color_mag.cpb";
-      detector = std::make_shared<acf::Detector>(sModel);
+      detector = std::make_shared<acf::Detector>(acfModel);
 
       if (detector.get() && detector->good()) {
         detector->setDoNonMaximaSuppression(true);
       }
-
     }
 
   	~ACFdetector()
@@ -75,46 +87,27 @@ public:
       // get width and height of 2D point cloud data
       int width = pCloud.width;
       int height = pCloud.height;
-
       // Convert from u (column / width), v (row/height) to position in array
       // where X,Y,Z data starts
       int arrayPosition = v*pCloud.row_step + u*pCloud.point_step;
-
       // compute position in array where x,y,z data start
       int arrayPosX = arrayPosition + pCloud.fields[0].offset; // X has an offset of 0
       int arrayPosY = arrayPosition + pCloud.fields[1].offset; // Y has an offset of 4
       int arrayPosZ = arrayPosition + pCloud.fields[2].offset; // Z has an offset of 8
 
-      float X = 0.0;
-      float Y = 0.0;
-      float Z = 0.0;
+      float X = 0.0, Y = 0.0, Z = 0.0;
 
       memcpy(&X, &pCloud.data[arrayPosX], sizeof(float));
       memcpy(&Y, &pCloud.data[arrayPosY], sizeof(float));
       memcpy(&Z, &pCloud.data[arrayPosZ], sizeof(float));
 
-      // ROS_INFO("xx: %f", X);
-      // ROS_INFO("yy: %f", Y);
-      // ROS_INFO("zz: %f", Z);
+      // ROS_INFO("xx: %f", X); ROS_INFO("yy: %f", Y); ROS_INFO("zz: %f", Z);
 
       geometry_msgs::Point p;
-      p.x = X;
-      p.y = Y;
-      p.z = Z;
+      p.x = X; p.y = Y; p.z = Z;
 
       return p;
     }
-
-    // static void drawObjects(cv::Mat& canvas, const std::vector<cv::Rect>& objects)
-    // {
-    //   for (const auto& o : objects)
-    //   {
-    //     cv::rectangle(canvas, o, { 0, 255, 0 }, 2, 8);
-
-    //     Point center = (o.tl()+o.br())/2;
-    //       circle(canvas, center, 2, Scalar(0,0,255), 2, 8, 0);
-    //   }
-    // }
 
     cv::Rect enlargeBBX(cv::Rect o, int cols, int rows)
     {
@@ -160,16 +153,140 @@ public:
       // ROS_INFO("width: %d", o.width);
       // ROS_INFO("height: %d", o.height);
 
-      // ROS_INFO("-------------------------------");
-
       // ROS_INFO("x: %d", large_bbx.x);
       // ROS_INFO("y: %d", large_bbx.y);
       // ROS_INFO("width: %d", large_bbx.width);
       // ROS_INFO("height: %d", large_bbx.height);
-
-      // ROS_INFO("-------------------------------");
-
       return large_bbx;
+    }
+
+    cv::Mat get_color_histogram(cv::Mat img){
+        cv::Mat img_hsv;
+        cvtColor(img, img_hsv, CV_BGR2HSV);
+        vector<Mat> hsv_planes;
+        split(img_hsv, hsv_planes);
+
+        Mat hist_h_mat, hist_s_mat, hist_v_mat;
+        int hSize = 181, svSize = 256;
+        float hrange[] = {0, 180}, svrange[] = {0, 256};
+        const float* hRange = {hrange};
+        const float* svRange = {svrange};
+
+        calcHist(&hsv_planes[0], 1, 0, Mat(), hist_h_mat, 1, &hSize, &hRange, true, false);
+        hist_h_mat.convertTo(hist_h_mat, CV_32FC1);
+        hist_h_mat = hist_h_mat / width / height;
+
+        calcHist(&hsv_planes[1], 1, 0, Mat(), hist_s_mat, 1, &svSize, &svRange, true, false);
+        hist_s_mat.convertTo(hist_s_mat, CV_32FC1);
+        hist_s_mat = hist_s_mat / width / height;
+
+        calcHist(&hsv_planes[2], 1, 0, Mat(), hist_v_mat, 1, &svSize, &svRange, true, false);
+        hist_v_mat.convertTo(hist_v_mat, CV_32FC1);
+        hist_v_mat = hist_v_mat / width / height;
+
+        cv::Mat hist;
+        vconcat(hist_h_mat, hist_s_mat, hist);
+        vconcat(hist, hist_v_mat, hist);
+        transpose(hist, hist);
+
+        return hist;
+    }
+
+    void applyDetector(AcfPtr detector, cv::Mat origImg)
+    {
+      std::vector<double> scores;
+      std::vector<cv::Rect> objects;
+      string color[6] = {"yellow", "green", "orange", "red", "blue", "purple"};
+      std::vector<string> colorVec(color, color+6);
+
+      (*detector)(origImg, objects, &scores);
+
+      int ind = 0;
+      std::stringstream ss;
+      for (const auto& o : objects)
+      {
+        acfDetector::detectedObj object_pub;
+
+        // make sure the bbx don't out of the image
+        if (o.x >= 0 && o.y >= 0 && (o.x + o.width) <= origImg.cols && (o.y + o.height) <= origImg.rows)
+        {
+          // draw bbbx
+          cv::rectangle(resultImg, o, { 0, 255, 0 }, 2, 8);
+          Point center = (o.tl()+o.br())/2;
+          circle(resultImg, center, 2, Scalar(0,0,255), 2, 8, 0);
+
+          // apply SVM color classifier
+          cv::Mat bbx_img;
+          std::vector<float> hist;
+          bbx_img = origImg(o);
+          cv::resize(bbx_img, bbx_img, cv::Size(width, height));
+          hist = ACFdetector::get_color_histogram(bbx_img);
+          int response = svm->predict(hist);
+          // cout << colorVec[response] << endl;
+          // imshow("bbx", bbx_img);
+          // waitKey(5);
+
+          // put the score and color on the bbx
+          ss << (int)scores[ind];
+          // cout << scores[ind] << endl;
+          cv::putText(resultImg, ss.str() + ", " + colorVec[response], cv::Point(o.x, o.y), CV_FONT_HERSHEY_SIMPLEX, 0.8, {255, 255, 255});
+          ind++;
+
+          cv::Rect large_o = ACFdetector::enlargeBBX(o, origImg.cols, origImg.rows);
+          cv::Mat obj_img;
+          obj_img = origImg(large_o);
+
+          // imshow("Cropped object image", obj_img);
+
+          sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", obj_img).toImageMsg();
+
+          // obj_img_pub.publish(img_msg);
+          object_pub.img = *img_msg;
+
+          // get the pos of the object
+          geometry_msgs::Point pos;
+
+          if (pCloud_cam.width != 0)
+          {
+            pos = pixelTo3DPoint(pCloud_cam, center.x, center.y);
+            if (isnan(pos.x))
+            {
+              // get other 4 pixels around center
+              for (int j=-6; j<7; j=j+4)
+              {
+                pos = pixelTo3DPoint(pCloud_cam, center.x + j, center.y);
+                if (!isnan(pos.x))
+                  break;
+
+              for (int k=-6; j<7; k=k+4)
+              {
+                pos = pixelTo3DPoint(pCloud_cam, center.x, center.y + k);
+
+                if (!isnan(pos.x))
+                  break;
+              }
+              if (!isnan(pos.x))
+                break;
+              }
+            }
+
+            // ROS_INFO("center x: %d", center.x);
+            // ROS_INFO("center y: %d", center.y);
+            // ROS_INFO("final_x: %f", pos.x);
+            // ROS_INFO("final_y: %f", pos.y);
+            // ROS_INFO("final_z: %f", pos.z);
+          }
+
+          // obj_pos_pub.publish(pos);
+          object_pub.pos = pos;
+          std_msgs::String color;
+          color.data = 'red';
+          object_pub.color = color;
+          object_pub.objClass = 1;
+
+          obj_detected_pub.publish(object_pub);
+        }
+      }
     }
 
   	void detection(const sensor_msgs::ImageConstPtr& msg)
@@ -177,108 +294,29 @@ public:
   		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
 
       cv::Mat origImg;
-      cv::Mat result;
       origImg = cv_ptr->image;
 
-      std::vector<double> scores;
-      std::vector<cv::Rect> objects;
-
-      if (origImg.empty())
-      {
-        ROS_INFO("empty image !!!!!!");
-      }
+      if (origImg.empty()) {ROS_INFO("empty image !!!!!!");}
       else
       {
-        (*detector)(origImg, objects, &scores);
+        resultImg = origImg.clone();
 
-        result = origImg.clone();
+        ACFdetector::applyDetector(detector, origImg);
 
-        // draw bounding boxes
-        for (const auto& o : objects)
-        {
-          cv::rectangle(result, o, { 0, 255, 0 }, 2, 8);
-          Point center = (o.tl()+o.br())/2;
-          circle(result, center, 2, Scalar(0,0,255), 2, 8, 0);
-
-
-          // ROS_INFO("x: %d", o.x);
-          // ROS_INFO("y: %d", o.y);
-          ROS_INFO("width: %d", o.width);
-          ROS_INFO("height: %d", o.height);
-
-          // make sure the bbx don't out of the image
-          if (o.x >= 0 && o.y >= 0 && (o.x + o.width) <= origImg.cols && (o.y + o.height) <= origImg.rows)
-          {
-
-            cv::Rect large_o = ACFdetector::enlargeBBX(o, origImg.cols, origImg.rows);
-            cv::Mat obj_img;
-            obj_img = origImg(large_o);
-
-            // imshow("Cropped object image", obj_img);
-
-            sensor_msgs::ImagePtr img_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", obj_img).toImageMsg();
-
-            obj_img_pub.publish(img_msg);
-
-            // get the pos of the object
-            geometry_msgs::Point pos;
-
-
-
-            if (pCloud_cam.width != 0)
-            {
-              pos = pixelTo3DPoint(pCloud_cam, center.x, center.y);
-              if (isnan(pos.x))
-              {
-                // get other 4 pixels around center
-                for (int j=-6; j<7; j=j+4)
-                {
-                  pos = pixelTo3DPoint(pCloud_cam, center.x + j, center.y);
-                  if (!isnan(pos.x))
-                    break;
-
-                for (int k=-6; j<7; k=k+4)
-                {
-                  pos = pixelTo3DPoint(pCloud_cam, center.x, center.y + k);
-
-                  if (!isnan(pos.x))
-                    break;
-                }
-                if (!isnan(pos.x))
-                  break;
-                }
-              }
-
-              ROS_INFO("center x: %d", center.x);
-              ROS_INFO("center y: %d", center.y);
-              ROS_INFO("final_x: %f", pos.x);
-              ROS_INFO("final_y: %f", pos.y);
-              ROS_INFO("final_z: %f", pos.z);
-            }
-
-            obj_pos_pub.publish(pos);
-          }
-
-        }
-
-        // drawObjects(result, objects);
-
-        // obj_pos_pub.publish(pos);
-        // imshow("Original image", origImg);
-
-        imshow("Detected image", result);
-        cv::waitKey(10);
+        imshow("Detected image", resultImg);
+        cv::waitKey(5);
       }
 
   	}
-
-
 
 private:
 	cv_bridge::CvImagePtr cv_ptr;
   sensor_msgs::PointCloud2 pCloud_cam;
 
   AcfPtr detector;
+  // Ptr<SVM> svm;
+
+  cv::Mat resultImg;
 
 };
 
