f703a88d6d3b1a9985e54de81e4c81cdc0292d58
jtao <jtao>
2018-11-22 23:52:23 +0100

    obstacle done

--- vision/detection/obstacle_detect/src/obstacle_detect.cpp
+++ vision/detection/obstacle_detect/src/obstacle_detect.cpp
@@ -25,20 +25,83 @@ public:
   ObjectDetection()
       : it_(nh_)
   {
-    namedWindow("Original image");
     namedWindow("Filtered image");
     frame_acc = 0;
 
     depth_image_sub = it_.subscribe("/camera/depth/image_raw", 1, &ObjectDetection::depthImageCallBack, this);
-
     obstale_detect_pub = n.advertise<std_msgs::Bool>("/obstacle/flag", 1);
 
+    // Parameter setting
     num = 0;
+    distThre = 210;
+    numThre = 5000;
   }
 
   ~ObjectDetection()
   {
     destroyAllWindows();
+  }
+
+  void meanValue(Mat img, float &mean, int &num){
+    float sum = 0;
+    float pixelValue;
+    for (int i=0; i<=img.rows; i++){
+      for (int j=0; j<=img.cols; j++){
+        pixelValue = img.at<unsigned short>(i, j);
+
+        if (pixelValue == 0)
+          continue;
+        else{
+          sum = sum + pixelValue;
+          num = num + 1;
+        }
+      }
+    }
+    mean = sum / num;
+
+    // img.setTo(0, img > 230);
+    // medianBlur(img, img, 5);
+
+    float depth;
+    int numObstcale = 0;
+    int first = -1, last = -1;
+    sum = 0;
+    for (int j=0; j<=img.cols; j++){
+        pixelValue = img.at<unsigned short>(10, j);
+        if (pixelValue == 0 || pixelValue > 230){
+          if (last == -2)
+            last = j;
+          continue;
+        }
+
+        else{
+          sum = sum + pixelValue;
+          numObstcale = numObstcale + 1;
+
+          if (first == -1)
+          {
+            first = j;
+            ROS_INFO("LALALALALA, %d", j);
+          }
+
+          last = -2;
+        }
+      }
+    if (last == -2)
+      last = img.cols;
+    depth = sum / numObstcale;
+
+    ROS_INFO("depth: %f", depth);
+    ROS_INFO("left: %d", first);
+    ROS_INFO("right: %d", last);
+  }
+
+  void obstacle_Pos_estimation(Mat img, float &depth, float &leftDist, float &rightDist){
+
+    // img.setTo(0, img > 230);
+    depth = 1;
+    leftDist = 1;
+    rightDist = 1;
   }
 
   void depthImageCallBack(const sensor_msgs::ImageConstPtr& msg)
@@ -48,67 +111,81 @@ public:
 
     Mat depthImage = origImg.clone();
 
-    depthImage.setTo(2000, depthImage < 10);
-    double min;
-    double max;
-
+    // Preprocess the depth image
+    depthImage.setTo(0, depthImage < 10);
     medianBlur ( depthImage, depthImage, 5);
-    cv::minMaxIdx(depthImage, &min, &max);
 
     // ROS_INFO("min: %f", min);
     // ROS_INFO("max: %f", max);
 
-
     flag.data = false;
 
-    if (min < 150)
-    {
-      flag.data = true;
-      frame_acc++;
-      // ROS_INFO("frame_acc: %d", frame_acc);
-    }
-
-    if (flag.data == false)
-    {
-      frame_acc = 1;
-    }
-
-    if (frame_acc >= 8)
-    {
-      flag.data = true;
-    }
-    else
-    {
-      flag.data = false;
-    }
-
-
+    // if (min < 150)
+    // {
+    //   flag.data = true;
+    //   frame_acc++;
+    //   // ROS_INFO("frame_acc: %d", frame_acc);
+    // }
+    //
+    // if (flag.data == false)
+    // {
+    //   frame_acc = 1;
+    // }
+    //
+    // if (frame_acc >= 8)
+    // {
+    //   flag.data = true;
+    // }
+    // else
+    // {
+    //   flag.data = false;
+    // }
     obstale_detect_pub.publish(flag);
+
+
+    // Visualize depth image
     cv::Mat adjMap;
+    double min;
+    double max;
+    cv::minMaxIdx(depthImage, &min, &max);
     // expand your range to 0..255. Similar to histEq();
     depthImage.convertTo(adjMap, CV_8UC1, 255 / (max-min), -min);
-
-// this is great. It converts your grayscale image into a tone-mapped one,
-// much more pleasing for the eye
-// function is found in contrib module, so include contrib.hpp
-// and link accordingly
     cv::Mat falseColorsMap;
     applyColorMap(adjMap, falseColorsMap, cv::COLORMAP_AUTUMN);
-
-    rectangle(falseColorsMap, Point(0, 230), Point(640, 250), Scalar(255,255,255), 2, 8, 0);
-    Mat cropedImage = origImg(Rect(0, 230, 640, 20));
-    imshow("croped image", cropedImage);
-    cv::waitKey(2);
-
-    num = num + 1;
-    if (num == 20){
-      cout << cropedImage << endl;
-      cout << "------------------------------------" << endl;
-      num = 0;
-    }
-
-
+    rectangle(falseColorsMap, Point(0, 260), Point(640, 280), Scalar(255,255,255), 2, 8, 0);
     cv::imshow("Filtered image", falseColorsMap);
+    waitKey(2);
+
+    // detect obstacle
+    Mat cropedImage = origImg(Rect(0, 260, 640, 20));
+    float mean;
+    int num;
+    meanValue(cropedImage, mean, num);
+    ROS_INFO("mean value: %f", mean);
+    ROS_INFO("num of pixels: %d", num);
+
+    if (mean < distThre)
+      ROS_INFO("Obstacle in front!!");
+
+    if (num < numThre)
+      ROS_INFO("Black side in front!!");
+
+    // float depth, leftDist, rightDist;
+    // obstacle_Pos_estimation(cropedImage, depth, leftDist, rightDist);
+
+
+    // num = num + 1;
+    // if (num == 20){
+    //   cout << cropedImage << endl;
+    //   cout << "------------------------------------" << endl;
+    //   num = 0;
+    //   cout << "mean value:" << endl;
+    //   cout << meanValue(cropedImage) << endl;
+    //   cout << "------------------------------------" << endl;
+    // }
+
+
+
 
       // Mat element = getStructuringElement(0, Size(5, 5), Point(5, 5));
       // morphologyEx(falseColorsMap, falseColorsMap, 2, element);
@@ -120,28 +197,10 @@ public:
 
     // cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::TYPE_16UC1);
     // cv::normalize(cv_ptr->image, cv_ptr->image, 0, 255, cv::NORM_MINMAX);
-    imshow("Original image", origImg);
-    cv::waitKey(3);
 
-    // cout << origImg << endl;
-    // cout << "------------------------------------" << endl;
   }
 
 
-  // bool obstacleDetected()
-  // {
-  //   //geometry_msgs::Point p;
-  //
-  //   int y = camera_height / 2;
-  //
-  //   for (int x = 0; x < camera_width; x++)
-  //   {
-  //     p = pixelTo3DPoint(pCloud_cam, x, y);
-  //     ROS_INFO("Depth: %f", p.z);
-  //   }
-  //
-  //   return false;
-  // }
 
 private:
   cv_bridge::CvImagePtr cv_ptr;
@@ -149,6 +208,8 @@ private:
   std_msgs::Bool flag;
   // sensor_msgs::PointCloud2 pCloud_cam;
   int num;
+  int numThre;
+  float distThre;
 };
 
 int main(int argc, char **argv)
